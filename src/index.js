6316b49127557df11fed103cd4448f007fedef62
calvsett2 <calvsett2@gmail.com>
2017-12-11 19:53:00 -0800

    huge component needs to be extracted

--- src/index.js
+++ src/index.js
@@ -3,6 +3,42 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+// example splitting large components into smaller components
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function formatDate(date){
+    return date.toLocaleDateString();
+}
+
+function Component(props){
+    return (
+        <div className="Comment">
+            <div className="UserInfo">
+                <img className="Avatar" 
+                    src={props.author.avatarUrl}
+                    alt={props.author.name}
+                />
+                <div className="UserInfo-name">
+                    {props.author.name}
+                </div>
+            </div>
+            <div className="Comment-text">
+                {props.text}
+            </div>
+            <div className="Comment-date">
+                {formatDate(props.date)} 
+            </div>
+        </div>
+    )
+}
+
+let props = {
+         author: {
+           avatarUrl: "https://www.pinterest.com/pin/594686325765703991/", 
+           name: "Calvin Settachatgul",
+         }, 
+         text : "something something somehting this is text", 
+         date : new Date()
+}
+
+ReactDOM.render(<Component {...props}/>, document.getElementById('root'));
 registerServiceWorker();
