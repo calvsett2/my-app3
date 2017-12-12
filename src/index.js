d68f45687660e8da9ac2f8929f4078998a89fae1
calvsett2 <calvsett2@gmail.com>
2017-12-11 18:43:03 -0800

    complete class component rendering

--- src/index.js
+++ src/index.js
@@ -4,5 +4,13 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Welcome extends React.Component{
+    render(){
+        return <h1>Hello, {this.props.name}</h1>;
+    }
+}
+
+const element = <Welcome name="Calvin" />;
+
+ReactDOM.render(element, document.getElementById('root'));
 registerServiceWorker();
