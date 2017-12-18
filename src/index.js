e9396b04b214deeef5e36f0520677055105f1a69
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2017-12-17 19:17:28 -0800

    converting functional component to class component

--- src/index.js
+++ src/index.js
@@ -9,13 +9,25 @@ registerServiceWorker();
 
 // encapsulating how the clock looks
 
-function Clock(props){
-    return (
-        <div>
-            <h1>Hello, world!</h1>
-            <h2>It is {props.date.toLocaleTimeString()}.</h2>
-        </div>
-    )
+// function Clock(props){
+//     return (
+//         <div>
+//             <h1>Hello, world!</h1>
+//             <h2>It is {props.date.toLocaleTimeString()}.</h2>
+//         </div>
+//     )
+// }
+
+class Clock extends React.Component{
+    render(){
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+            </div>
+        )
+
+    } 
 }
 
 function tick() {
