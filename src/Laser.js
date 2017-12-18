6bbca1f6c61232fd043506adc31f7aae5e0747e7
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2017-12-18 11:42:29 -0800

    handling a click event on a <p> element

--- src/Laser.js
+++ src/Laser.js
@@ -3,17 +3,20 @@ import React, { Component } from 'react';
 class Laser extends Component {
     constructor(props){
         super(props)
-        this.state= {laserOn: false};
+        this.state= {laserOn: "laseroff"};
+        this.switchLaser = this.switchLaser.bind(this);
     }
     
     switchLaser(){
-        this.setState(
+        this.setState( {laserOn:  "laseron"}
         )
+        console.log("Laser was fired!");
     }
     render(){
         return (
             <div className="laser">
-                This is my laser.
+                This is my laser. laserOn: {this.state.laserOn}.
+                <p onClick={this.switchLaser}>Fire the Laser!</p>
             </div>
         )
     }
