1eb29a6bf8a0a77d3e7c36b968aafbd0939ac19b
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2017-12-18 16:09:38 -0800

    adding todo

--- src/Todos.js
+++ src/Todos.js
@@ -4,18 +4,25 @@ let all_todos = [
                     {   id:1, title: "title1"}, 
                     {   id:2, title: "title2"}, 
                     {   id:3, title: "title3"}, 
-                    {   id:4, title: "title4"}, 
+                    {   id:4, title: "title4"}
                 ]
 
 class Todos extends Component{
     constructor(props){
         super(props)
-        this.state = {all_todos}
+        this.state = {all_todos: all_todos}
     }
     render(){
-        return (
-        <div>this is todos component</div>
-        )
+        let html_string = (<div className="Todos">
+                            <ul>
+                                <li>Todos</li>
+                            </ul>
+                           </div>)
+                    
+               
+        
+        return html_string;
+        
     }
 
 }
